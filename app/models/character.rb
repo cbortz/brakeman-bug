f2f1e47248a62734efeb8a440f3839eb8ae11a96
Chet Bortz <cbortz@users.noreply.github.com>
2019-11-21 16:42:40 -0500

    Add examples

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,25 @@
 class Character < ApplicationRecord
+  def self.scope_with_strip_heredoc(name)
+    conditions = <<-SQL.strip_heredoc
+      name = '#{name}'
+    SQL
+
+    where(conditions)
+  end
+
+  def self.scope_with_standard_heredoc(name)
+    conditions = <<-SQL
+      name = '#{name}'
+    SQL
+
+    where(conditions)
+  end
+
+  def self.scope_with_squiggle_heredoc(name)
+    conditions = <<~SQL
+      name = '#{name}'
+    SQL
+
+    where(conditions)
+  end
 end
